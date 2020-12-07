857c2a0df88175ce7c87e3b1dc77fd0875a524d6
anngo14 <31315148+anngo14@users.noreply.github.com>
2020-12-06 22:13:57 -0600

    changed nav menu location and footer bg color

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,10 +3,15 @@ import '../css/Nav.css'
 export default function Nav() {
     return (
         <div className='nav-container'>
-            <div className='burger'>
-                <div className='line'></div>
-                <div className='line'></div>
-                <div className='line'></div>
+            <div className='nav-btn'>
+                <div className='burger'>
+                    <div className='line'></div>
+                    <div className='line'></div>
+                    <div className='line'></div>
+                </div>
+                <div>
+                    <span id='nav-menu-title'>MENU</span>
+                </div>
             </div>
             <div className='nav-menu'>
                 <a href='#' className='nav-link'>Home</a>

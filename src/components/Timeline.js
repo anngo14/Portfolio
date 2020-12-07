1b4bcc6942302965c9fc30cec62448ba87cb3a91
anngo14 <31315148+anngo14@users.noreply.github.com>
2020-12-06 22:50:03 -0600

    added initial timeline layout

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -5,6 +5,11 @@ export default function Timeline() {
     return (
         <div className='timeline-container'>
             <h1>Timeline</h1>
+            <div className='timeline-content'>
+                <span className='timeline-end'>2010</span>
+                <div className='timeline'></div>
+                <span className='timeline-end'>2020</span>
+            </div>
         </div>
     )
 }

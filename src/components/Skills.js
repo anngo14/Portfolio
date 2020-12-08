cd531e5f5b44681ce4c5c7471f51cfe64f722854
anngo14 <31315148+anngo14@users.noreply.github.com>
2020-12-07 20:28:42 -0600

    added shelves to Skills component

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,6 +5,66 @@ export default function Skills() {
     return (
         <div className='skills-container'>
             <h1>Skills</h1>
+            <div className='shelf-container'>
+                <h2>Programming Languages</h2>
+                <div className='shelf-content'>
+                    <div className='shelf-item'>
+                        <div className='shelf-icon'>
+                        </div>
+                        <span className='shelf-title'>Java</span>
+                    </div>
+                    <div className='shelf-item'>
+                        <div className='shelf-icon'>
+                        </div>
+                        <span className='shelf-title'>C</span>
+                    </div>
+                    <div className='shelf-item'>
+                        <div className='shelf-icon'>
+                        </div>
+                        <span className='shelf-title'>JS</span>
+                    </div>
+                </div>
+            </div>
+            <div className='shelf-container'>
+                <h2>Databases</h2>
+                <div className='shelf-content'>
+                    <div className='shelf-item'>
+                        <div className='shelf-icon'>
+                        </div>
+                        <span className='shelf-title'>MySQL</span>
+                    </div>
+                    <div className='shelf-item'>
+                        <div className='shelf-icon'>
+                        </div>
+                        <span className='shelf-title'>PostgreSQL</span>
+                    </div>
+                    <div className='shelf-item'>
+                        <div className='shelf-icon'>
+                        </div>
+                        <span className='shelf-title'>MongoDB</span>
+                    </div>
+                </div>
+            </div>
+            <div className='shelf-container'>
+                <h2>Databases</h2>
+                <div className='shelf-content'>
+                    <div className='shelf-item'>
+                        <div className='shelf-icon'>
+                        </div>
+                        <span className='shelf-title'>Angular</span>
+                    </div>
+                    <div className='shelf-item'>
+                        <div className='shelf-icon'>
+                        </div>
+                        <span className='shelf-title'>React</span>
+                    </div>
+                    <div className='shelf-item'>
+                        <div className='shelf-icon'>
+                        </div>
+                        <span className='shelf-title'>MongoDB</span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }

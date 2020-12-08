c9561640aa4713e4facbb663e53cbfb9899f5e00
Andrew <aandrewnngo@gmail.com>
2020-12-07 22:56:15 -0600

    added initial portfolio component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Footer from './components/Footer';
 import Experience from './components/Experience';
 import Skills from './components/Skills';
 import Contact from './components/Contact'
+import Portfolio from './components/Portfolio';
 
 function App(){
   return(
@@ -15,6 +16,7 @@ function App(){
       <About />
       <Experience />
       <Skills />
+      <Portfolio />
       <Contact />
       <Footer />
     </div>
---
+++ src/components/Portfolio.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../css/Portfolio.css'
+
+export default function Portfolio() {
+    return (
+        <div className='portfolio-container'>
+            <h1>Portfolio</h1>
+            <div className='portfolio-content'>
+
+            </div>
+        </div>
+    )
+}

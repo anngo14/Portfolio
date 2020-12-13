db473766030d5ec4c9dfa15646d3fc2eeb158ee6
Andrew <aandrewnngo@gmail.com>
2020-12-12 20:46:26 -0600

    added node server

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var path = require('path');
+
+const PORT = process.env.PORT || 5000;
+
+var app = express();
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
